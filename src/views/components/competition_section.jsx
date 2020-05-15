201959d47e2afa687e0480a69230038db5124e8e
yoloabhi <abhirup_cool2@yahoo.com>
2020-05-16 03:49:12 +0530

    changes to competittion section

--- src/views/components/competition_section.jsx
+++ src/views/components/competition_section.jsx
@@ -36,42 +36,32 @@ class Competition extends React.Component {
                         <Col xs="6" sm="4">
                             <Card>
                                 <CardImg top width="100%" src={require("./../../assets/img/logo.svg")} alt="Card image cap" />
+                                <CardBody>
+                                    <CardTitle>Card title</CardTitle>
+                                </CardBody>
 
                             </Card>
                         </Col>
                         <Col xs="6" sm="4">
                             <Card>
                                 <CardImg top width="100%" src={require("./../../assets/img/logo.svg")} alt="Card image cap" />
+                                <CardBody>
+                                    <CardTitle>Card title</CardTitle>
+                                </CardBody>
 
                             </Card>
                         </Col>
                         <Col sm="4">
                             <Card>
                                 <CardImg top width="100%" src={require("./../../assets/img/logo.svg")} alt="Card image cap" />
+                                <CardBody>
+                                    <CardTitle>Card title</CardTitle>
+                                </CardBody>
 
                             </Card>
                         </Col>
                     </Row>
-                    <Row>
-                        <Col xs="6" sm="4">
-                            <Card>
-                                <CardImg top width="100%" src={require("./../../assets/img/logo.svg")} alt="Card image cap" />
 
-                            </Card>
-                        </Col>
-                        <Col xs="6" sm="4">
-                            <Card>
-                                <CardImg top width="100%" src={require("./../../assets/img/logo.svg")} alt="Card image cap" />
-
-                            </Card>
-                        </Col>
-                        <Col sm="4">
-                            <Card>
-                                <CardImg top width="100%" src={require("./../../assets/img/logo.svg")} alt="Card image cap" />
-
-                            </Card>
-                        </Col>
-                    </Row>
                 </Container>
 
 
